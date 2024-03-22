2eba282cf2ce677a384ad00551df98288abb33e7
James Edmonson <james.edmonson@tcu.edu>
2024-03-22 10:47:04 -0500

    validation dependency

--- src/main/java/edu/tcu/cs/peerevaluation/student/dto/StudentDto.java
+++ src/main/java/edu/tcu/cs/peerevaluation/student/dto/StudentDto.java
@@ -1,11 +1,13 @@
 package edu.tcu.cs.peerevaluation.student.dto;
 
-//import jakarta.validation.constraints.NotEmpty;
-//this allows the '@NotEmpty modifier, but requires a dependency'
+import jakarta.validation.constraints.NotEmpty;
 
 public record StudentDto(String id,
+                         @NotEmpty(message = "first name is required.")
                          String firstName,
+                         @NotEmpty(message = "last name is required.")
                          String lastName,
+                         @NotEmpty(message = "email is required.")
                          String email) {
 
 }
