4071bb705946663b94e862eead6b0f39bf33f850
Sean Wymer <sean.wymer@tcu.edu>
2024-04-03 17:13:51 -0500

    added Instructor class

---
+++ src/main/java/edu/tcu/cs/peerevaluation/instructor/Instructor.java
@@ -0,0 +1,77 @@
+package edu.tcu.cs.peerevaluation.instructor;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Instructor implements Serializable {
+
+    @Id
+    private String id;
+
+    private String firstName;
+
+    private String middleInitial;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    public Instructor() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+
+}
