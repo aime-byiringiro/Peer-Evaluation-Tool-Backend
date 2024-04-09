18811ac49cf73074bee52516cba255ec7cfccc72
Harrison Leath <hleath@me.com>
2024-04-08 23:14:43 -0500

    Added Component annotation to SectionDtoToSectionConverter

--- src/main/java/edu/tcu/cs/peerevaluation/section/converter/SectionDtoToSectionConverter.java
+++ src/main/java/edu/tcu/cs/peerevaluation/section/converter/SectionDtoToSectionConverter.java
@@ -3,7 +3,9 @@ package edu.tcu.cs.peerevaluation.section.converter;
 import edu.tcu.cs.peerevaluation.section.Section;
 import edu.tcu.cs.peerevaluation.section.dto.SectionDto;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SectionDtoToSectionConverter implements Converter<SectionDto, Section> {
 
     @Override
