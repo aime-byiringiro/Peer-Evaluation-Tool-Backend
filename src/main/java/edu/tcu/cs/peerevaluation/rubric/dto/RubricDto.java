acaaa763ef72f65b7e411b4830644bcd0ea884fd
James Edmonson <james.edmonson@tcu.edu>
2024-04-08 22:54:53 -0500

    Fixing Aliya's Mistake p2

--- src/main/java/edu/tcu/cs/peerevaluation/rubric/dto/RubricDto.java
+++ src/main/java/edu/tcu/cs/peerevaluation/rubric/dto/RubricDto.java
@@ -3,6 +3,7 @@ package edu.tcu.cs.peerevaluation.rubric.dto;
 import java.util.List;
 
 import edu.tcu.cs.peerevaluation.rubric.criterion.Criterion;
+import edu.tcu.cs.peerevaluation.rubric.criterion.dto.CriterionDto;
 
 
 public record RubricDto(String rubricName,
