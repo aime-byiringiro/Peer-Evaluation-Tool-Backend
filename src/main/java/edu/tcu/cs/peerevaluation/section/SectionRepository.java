90efeffb81eb81912bdcc16a683217e9c2ee2a03
aimebyiringiro <byiringiroaimeismael@gmail.com>
2024-04-12 15:46:03 -0500

    sectionrepository

--- src/main/java/edu/tcu/cs/peerevaluation/section/SectionRepository.java
+++ src/main/java/edu/tcu/cs/peerevaluation/section/SectionRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SectionRepository extends JpaRepository<Section, String>, JpaSpecificationExecutor<Section> {
 
+
+
 }
