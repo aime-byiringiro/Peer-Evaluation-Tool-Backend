9fcab8cffc74aa691589ac4bab3a7e520bef52ec
aimebyiringiro <byiringiroaimeismael@gmail.com>
2024-04-12 10:52:47 -0500

    updating sectioncontroller

--- src/main/java/edu/tcu/cs/peerevaluation/section/SectionController.java
+++ src/main/java/edu/tcu/cs/peerevaluation/section/SectionController.java
@@ -1,24 +1,40 @@
 package edu.tcu.cs.peerevaluation.section;
 
+import edu.tcu.cs.peerevaluation.section.converter.SectionDtoToSectionConverter;
+import edu.tcu.cs.peerevaluation.section.converter.SectionToSectionDtoConverter;
+import edu.tcu.cs.peerevaluation.section.dto.SectionDto;
 import edu.tcu.cs.peerevaluation.system.Result;
 import edu.tcu.cs.peerevaluation.system.StatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Statement;
 
+
+@RestController
+@RequestMapping("/section")
 public class SectionController {
-
     private final SectionService sectionService;
-    public SectionController(SectionService sectionService) {
+    private final SectionDtoToSectionConverter sectionDtoToSectionConverter;
 
+    private final SectionToSectionDtoConverter  sectionToSectionDtoConverter;
+
+    public SectionController(SectionService sectionService, SectionDtoToSectionConverter sectionDtoToSectionConverter, SectionToSectionDtoConverter sectionToSectionDtoConverter) {
         this.sectionService = sectionService;
+        this.sectionDtoToSectionConverter = sectionDtoToSectionConverter;
+        this.sectionToSectionDtoConverter = sectionToSectionDtoConverter;
     }
-    @GetMapping("#/{sectionId") //TBD
+
+
+    @GetMapping("/{sectionId") 
     public Result findSectionById(@PathVariable String sectionId){
         Section foundSection = this.sectionService.adminFindsSeniorDesignSectionsById(sectionId);
-        return new Result(true, StatusCode.SUCCESS, "Find One Success", foundSection);
-    }
+        SectionDto sectionDto = this.sectionToSectionDtoConverter.convert(foundSection);
+        return  new Result(true, StatusCode.SUCCESS, "Find One Success", sectionDto);
 
+
+    }
 
 }
--- src/main/java/edu/tcu/cs/peerevaluation/section/SectionRepository.java
+++ src/main/java/edu/tcu/cs/peerevaluation/section/SectionRepository.java
@@ -1,9 +1,10 @@
 package edu.tcu.cs.peerevaluation.section;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SectionRepository extends JpaRepository<Section, String> {
+public interface SectionRepository extends JpaRepository<Section, String>, JpaSpecificationExecutor<Section> {
 
 }
