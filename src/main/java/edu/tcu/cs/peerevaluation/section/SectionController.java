13728e9be4bdb28c8a008c73eba0a69f77903caf
aime-byiringiro <aime.byiringiro@tcu.edu>
2024-04-15 15:35:07 -0500

    stating create new section

--- src/main/java/edu/tcu/cs/peerevaluation/section/SectionController.java
+++ src/main/java/edu/tcu/cs/peerevaluation/section/SectionController.java
@@ -5,10 +5,8 @@ import edu.tcu.cs.peerevaluation.system.StatusCode;
 import edu.tcu.cs.peerevaluation.section.converter.SectionDtoToSectionConverter;
 import edu.tcu.cs.peerevaluation.section.converter.SectionToSectionDtoConverter;
 import edu.tcu.cs.peerevaluation.section.dto.SectionDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.sql.Statement;
@@ -34,6 +32,18 @@ public class SectionController {
         Section foundSection = this.sectionService.adminFindsSeniorDesignSectionsBySectionName(sectionID);
         SectionDto sectionDto = this.sectionToSectionDtoConverter.convert(foundSection); // convert the json section into section object
         return new Result(true, StatusCode.SUCCESS, "Find Success", sectionDto);
+
+    }
+
+
+
+    @PostMapping
+    public Result createNewSection(@Valid @RequestBody SectionDto sectionDto) {
+
+        Section newSection = this.sectionDtoToSectionConverter.convert(sectionDto);
+        Section savedSection = this.sectionService.save(newSection);
+        SectionDto savedSectionDto = this.sectionToSectionDtoConverter.convert(savedSection);
+        return new Result(true, StatusCode.SUCCESS, "Add Success", savedSectionDto);
 
     }
 
