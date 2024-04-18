902c8420eb87015acc6392b5ddce0eba877c8c14
James Edmonson <james.edmonson@tcu.edu>
2024-04-18 09:43:29 -0500

    add edit and delete submissions

--- src/main/java/edu/tcu/cs/peerevaluation/war/WARController.java
+++ src/main/java/edu/tcu/cs/peerevaluation/war/WARController.java
@@ -3,10 +3,30 @@ package edu.tcu.cs.peerevaluation.war;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.tcu.cs.peerevaluation.peerEvalUser.MyUserPrincipal;
+import edu.tcu.cs.peerevaluation.student.Student;
+import edu.tcu.cs.peerevaluation.system.Result;
+import edu.tcu.cs.peerevaluation.system.StatusCode;
 import edu.tcu.cs.peerevaluation.war.converter.WARDtoToWARConverter;
 import edu.tcu.cs.peerevaluation.war.converter.WARToWARDtoConverter;
+import edu.tcu.cs.peerevaluation.war.submission.Submission;
 import edu.tcu.cs.peerevaluation.war.submission.converter.SubmissionDtoToSubmissionConverter;
 import edu.tcu.cs.peerevaluation.war.submission.converter.SubmissionToSubmissionDtoConverter;
+import edu.tcu.cs.peerevaluation.war.submission.dto.SubmissionDto;
+import jakarta.validation.Valid;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/war")
@@ -28,6 +48,51 @@ public class WARController {
     this.warToWARDtoConverter = warToWARDtoConverter;
     this.submissionDtoToSubmissionConverter = submissionDtoToSubmissionConverter;
     this.submissionToSubmissionDtoConverter = submissionToSubmissionDtoConverter;
+  }
+
+  //findByActiveWeek
+  @GetMapping("/{activeWeek}")
+  public Result getMethodName(@PathVariable String activeWeek) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    MyUserPrincipal principal = (MyUserPrincipal) authentication.getPrincipal();
+    Student loggedInStudent = principal.getPeerEvalUser().getStudent(); 
+
+    List<Submission> foundSubmissions = this.warService.findByWeekAndStudent(loggedInStudent.getId(), activeWeek);
+    List<SubmissionDto> foundDtos = foundSubmissions.stream()
+            .map(foundSubmission -> this.submissionToSubmissionDtoConverter.convert(foundSubmission))
+            .collect(Collectors.toList());
+    return new Result(true, StatusCode.SUCCESS, "find by week success", foundDtos);
+  }
+
+  //addSubmission
+  @PostMapping("/{week}")
+  public Result newSubmission(@PathVariable Integer week, @Valid @RequestBody SubmissionDto submissionDto) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    MyUserPrincipal principal = (MyUserPrincipal) authentication.getPrincipal();
+    Student loggedInStudent = principal.getPeerEvalUser().getStudent();
+    
+    Submission newSubmission = this.submissionDtoToSubmissionConverter.convert(submissionDto);
+    newSubmission.setWar(this.warService.findByWeekAndTeam(loggedInStudent.getTeam().getId(), week));
+    newSubmission.setTeamMember(loggedInStudent);
+    Submission savedSubmission = this.warService.saveSubmission(newSubmission);
+    SubmissionDto savedDto = this.submissionToSubmissionDtoConverter.convert(savedSubmission);
+    return new Result(true,StatusCode.SUCCESS,"update success", savedDto);
+  }
+
+  //editSubmission
+  @PutMapping
+  public Result updateSubmission(@Valid @RequestBody SubmissionDto submissionDto){
+    Submission update = this.submissionDtoToSubmissionConverter.convert(submissionDto);
+    Submission updatedSubmission = this.warService.update(submissionDto.id(),update);
+    SubmissionDto updatedDto = this.submissionToSubmissionDtoConverter.convert(updatedSubmission);
+    return new Result(true,StatusCode.SUCCESS,"add success", updatedDto);
+  }
+
+  //deleteSubmission
+  @DeleteMapping("/{submissionId}")
+  public Result deleteSubmission(@PathVariable Integer submissionId){
+    this.warService.deleteSubmission(submissionId);
+    return new Result(true,StatusCode.SUCCESS,"delete success");
   }
 
 }
