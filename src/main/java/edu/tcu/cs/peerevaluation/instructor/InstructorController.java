304a06dc7a145bf64e81bcf78f2a825e2824b768
Sean Wymer <sean.wymer@tcu.edu>
2024-04-19 10:11:43 -0500

    added import

--- src/main/java/edu/tcu/cs/peerevaluation/instructor/InstructorController.java
+++ src/main/java/edu/tcu/cs/peerevaluation/instructor/InstructorController.java
@@ -1,10 +1,13 @@
 package edu.tcu.cs.peerevaluation.instructor;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.tcu.cs.peerevaluation.instructor.converter.InstructorDtoToInstructorConverter;
 import edu.tcu.cs.peerevaluation.instructor.converter.InstructorToInstructorDtoConverter;
+import edu.tcu.cs.peerevaluation.system.Result;
 
 @RestController
 @RequestMapping("/instructor")
@@ -21,6 +24,13 @@ public class InstructorController {
     this.instructorToInstructorDtoConverter = instructorToInstructorDtoConverter;
     this.instructorDtoToInstructorConverter = instructorDtoToInstructorConverter;
   }
+
+  @GetMapping()
+  public Result findAllInstructors() {
+    List<Instructor> foundInstructors = this.instructorService.findAll();
+    return null;
+  }
+
 
   
 
