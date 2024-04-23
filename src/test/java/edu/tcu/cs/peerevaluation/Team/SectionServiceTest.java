d6cbbce6b38eef2b4089172e829d105c12eec942
aime-byiringiro <aime.byiringiro@tcu.edu>
2024-04-22 20:33:54 -0500

    testSaveSuccess passed

--- src/test/java/edu/tcu/cs/peerevaluation/Team/SectionServiceTest.java
+++ src/test/java/edu/tcu/cs/peerevaluation/Team/SectionServiceTest.java
@@ -1,37 +1,21 @@
 package edu.tcu.cs.peerevaluation.Team;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.tcu.cs.peerevaluation.rubric.Rubric;
-import edu.tcu.cs.peerevaluation.rubric.converter.RubricToRubricDtoConverter;
 import edu.tcu.cs.peerevaluation.rubric.criterion.Criterion;
-import edu.tcu.cs.peerevaluation.rubric.criterion.converter.CriterionToCriterionDtoConverter;
-import edu.tcu.cs.peerevaluation.rubric.criterion.dto.CriterionDto;
-import edu.tcu.cs.peerevaluation.rubric.dto.RubricDto;
 import edu.tcu.cs.peerevaluation.section.Section;
+import edu.tcu.cs.peerevaluation.section.utils.IdWorker;
+import edu.tcu.cs.peerevaluation.section.SectionController;
 import edu.tcu.cs.peerevaluation.section.SectionRepository;
 import edu.tcu.cs.peerevaluation.section.SectionService;
-
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import edu.tcu.cs.peerevaluation.section.dto.SectionDto;
-import edu.tcu.cs.peerevaluation.system.StatusCode;
 import edu.tcu.cs.peerevaluation.system.exception.ObjectNotFoundException;
+import edu.tcu.cs.peerevaluation.team.Team;
 import edu.tcu.cs.peerevaluation.team.TeamRepository;
 import edu.tcu.cs.peerevaluation.team.TeamService;
+import org.h2.command.dml.Update;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -41,15 +25,33 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-import javax.print.attribute.standard.Media;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 @ExtendWith(MockitoExtension.class)
 public class SectionServiceTest {
 
@@ -68,6 +70,41 @@ public class SectionServiceTest {
     }
 
 
+    @Test
+    void testSaveSuccess(){
+
+        Team newTeam = new Team();
+        newTeam.setId(10);
+        newTeam.setTeamName("PeerEvaluationTool");
+        Section newSection = new Section();
+        newSection.setId(10);
+        newSection.setSectionName("SectionName");
+        newSection.setAcademicYear("2022");
+        newSection.setFirstDay("09/05");
+        newSection.setLastDay("10/28");
+        Rubric newRubric = new Rubric();
+        newRubric.setRubricName("newRubric");
+        newRubric.setId(10);
+        List<Criterion> newCriterionList = new ArrayList<>();
+        Criterion criterion1 = new Criterion();
+        criterion1.setId(10);
+        criterion1.setCriterionName("criterionName");
+        criterion1.setDescription("hello hello");
+        criterion1.setMaxScore(10);
+        newCriterionList.add(criterion1);
+        newRubric.setCriterionList(newCriterionList);
+        newSection.setRubric(newRubric);
+        newTeam.setSection(newSection);
+
+        given(teamRepository.save(newTeam)).willReturn(newTeam);
+
+        Team savedTeam = this.teamService.save(newTeam);
+
+        assertThat(savedTeam.getId()).isEqualTo(10);
+        assertThat(savedTeam.getTeamName()).isEqualTo(newTeam.getTeamName());
+        assertThat(savedTeam.getSection()).isEqualTo(newTeam.getSection());
+
+    }
 
 
 }
