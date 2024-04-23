3c5212ebc34304ac76a58e2094e8aa24aa5b2d14
James Edmonson <james.edmonson@tcu.edu>
2024-04-22 22:44:43 -0500

    UH OH, Aliya made a itty bitty boo boo, let's fix that

--- src/main/java/edu/tcu/cs/peerevaluation/system/DBDataInitializer.java
+++ src/main/java/edu/tcu/cs/peerevaluation/system/DBDataInitializer.java
@@ -268,22 +268,10 @@ public class DBDataInitializer implements CommandLineRunner {
     i1u.setEnabled(true);
     i1u.setRoles("instructor user");
 
-    System.out.println(i1u.getPassword());
     userService.save(i1u);
-    System.out.println(i1u.getPassword());
     i1u.setInstructor(i1);
-    System.out.println(i1u.getPassword());
     userService.update(i1u.getId(),i1u);
-    System.out.println(i1u.getPassword());
-
     i1.setUser(i1u);
-    System.out.println(i1u.getPassword());
-   
-    System.out.println(i1u.getPassword());
-
-    
-    
-    //instructorRepository.save(i1);
     
 
     // --------------------------//
