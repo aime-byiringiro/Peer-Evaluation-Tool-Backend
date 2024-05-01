c16acd2f710d50c051ce9570090e2162210a70ca
Harrison Leath <hleath@me.com>
2024-05-01 10:36:12 -0500

    Find fully implemented for teams

--- src/main/java/edu/tcu/cs/peerevaluation/team/TeamSpecifications.java
+++ src/main/java/edu/tcu/cs/peerevaluation/team/TeamSpecifications.java
@@ -1,6 +1,7 @@
 package edu.tcu.cs.peerevaluation.team;
 
 import edu.tcu.cs.peerevaluation.instructor.Instructor;
+import edu.tcu.cs.peerevaluation.section.Section;
 import jakarta.persistence.criteria.Join;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,10 @@ public class TeamSpecifications {
     }
 
     public static Specification<Team> hasSectionName(String sectionName) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("sectionName"), "%" + sectionName + "%");
+        return (root, query, criteriaBuilder) -> {
+            Join<Team, Section> sectionJoin = root.join("section");
+            return criteriaBuilder.equal(sectionJoin.get("sectionName"), sectionName);
+        };
     }
 
     public static Specification<Team> hasAcademicYear(String academicYear) {
@@ -22,15 +26,15 @@ public class TeamSpecifications {
 
     public static Specification<Team> hasInstructorFirstName(String instructorFirstName) {
         return (root, query, criteriaBuilder) -> {
-            Join<Team, Instructor> instructorJoin = root.join("instructors");
-            return criteriaBuilder.equal(instructorJoin.get("last_name"), instructorFirstName);
+            Join<Team, Instructor> instructorJoin = root.join("instructor");
+            return criteriaBuilder.equal(instructorJoin.get("firstName"), instructorFirstName);
         };
     }
 
     public static Specification<Team> hasInstructorLastName(String instructorLastName) {
         return (root, query, criteriaBuilder) -> {
-            Join<Team, Instructor> instructorJoin = root.join("instructors");
-            return criteriaBuilder.equal(instructorJoin.get("last_name"), instructorLastName);
+            Join<Team, Instructor> instructorJoin = root.join("instructor");
+            return criteriaBuilder.equal(instructorJoin.get("lastName"), instructorLastName);
         };
     }
 }
